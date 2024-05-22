6f86cb1319cf181e79d1067e00e0f22f2007ccda
Lester Cordero <lester@boss.tech>
2024-05-21 23:46:04 -0600

    safe check 1

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,11 +3,15 @@ import { Theme } from "../types";
 import { useMediaQuery } from "./useMediaQuery";
 import { resolveStyleProps } from "../functions/transformers";
 
-export const useTheme = (theme: Theme) => {
-  const { resolveMediaQuery } = useMediaQuery(theme.schema.breakpoints);
+export const useTheme = (theme?: Theme) => {
+  const breakpoints = theme?.schema?.breakpoints;
+  const stylesSchema = theme?.schema?.styles;
+  const colorsSchema = theme?.schema?.colors;
 
-  const styles = useMemo(() => resolveMediaQuery(theme.schema.styles), [theme]);
-  const colors = useMemo(() => resolveStyleProps(theme.schema.colors), [theme]);
+  const { resolveMediaQuery } = useMediaQuery(breakpoints);
+
+  const styles = useMemo(() => stylesSchema ? resolveMediaQuery(stylesSchema) : {}, [stylesSchema, resolveMediaQuery]);
+  const colors = useMemo(() => colorsSchema ? resolveStyleProps(colorsSchema) : {}, [colorsSchema]);
 
   return {
     styles,
