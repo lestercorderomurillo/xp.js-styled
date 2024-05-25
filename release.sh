6781fc60885ba4a785eb6fcb7588c113fa05b96e
Lester Cordero <lester@boss.tech>
2024-05-24 18:47:16 -0600

    v0.1.2

--- release.sh
+++ release.sh
@@ -1,21 +1,17 @@
 # Build the project
 yarn build
 
-# Bump the version
-npm version patch
-
 # Add all changes to the Git index
 git add .
 
-# Commit the changes with the new version as the commit message
-NEW_VERSION=$(node -p -e "require('./package.json').version")
-git commit -m "release: $NEW_VERSION"
+# Bump the version
+yarn version --patch
 
 # Push the changes to the remote repository
 git push
 
 # Publish the package
-npm publish --access=public
+yarn publish --access public
 
 # Pull the changes from the remote repository
 git pull || exit 1
