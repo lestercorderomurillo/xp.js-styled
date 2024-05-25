0c6f87e3dc8d499961e4d092f5052323b6a55461
Lester Cordero <lester@boss.tech>
2024-05-25 14:45:39 -0600

    Fixed bugs

--- src/functions/createStyled.tsx
+++ src/functions/createStyled.tsx
@@ -1,6 +1,6 @@
 import React, { useMemo } from "react";
-import { useColorScheme, useWindowDimensions } from "react-native";
-import { StyledSchema } from "../types";
+import { View, useColorScheme, useWindowDimensions } from "react-native";
+import { StyledProps, StyledSchema } from "../types";
 import { deepMerge } from "../utils";
 import { deepTransform, media } from "./transformers";
 
@@ -10,15 +10,16 @@ import { deepTransform, media } from "./transformers";
  * @param {StyledSchema} schema The style schema.
  * @returns A HOC wrapper around your component with extended features.
  */
-export const createStyled = <TProps extends {}>(Component: React.ComponentType<any>, schema?: StyledSchema) => {
-  return ({ ...args }) => {
+export const createStyled = <TProps extends {}, TVariantNames extends string>(Component: React.ComponentType<any>, schema?: StyledSchema<{}, TVariantNames>) => {
+
+  return ({ ...args }: StyledProps<{}, {}, keyof typeof schema['variants']>) => {
     const deviceColorScheme = useColorScheme();
     const deviceDimensions = useWindowDimensions();
 
     const memo = useMemo(() => {
       const schemaStyle = deepTransform(media(schema ?? {}, schema?.theme?.breakpoints), schema?.theme);
       const style = deepTransform(args.style, schema?.theme);
-      const variantStyle = schema?.variants && schema?.variants[args.variant] ? deepTransform(schema?.variants[args.variant], schema?.theme) : {};
+      const variantStyle = schema?.variants && args?.variant && schema?.variants[args.variant] ? deepTransform(schema?.variants[args.variant], schema?.theme) : {};
 
       return {
         style,
--- src/functions/createTheme.ts
+++ src/functions/createTheme.ts
@@ -1,5 +1,10 @@
 import { ThemeSchema } from "../types";
 
+/**
+ * Function to create a theme based on a schema.
+ * @param schema - Schema with m edia query values.
+ * @returns The compiled schema for use into styled components.
+ */
 export const createTheme = (schema: ThemeSchema) => {
   return schema;
 };
