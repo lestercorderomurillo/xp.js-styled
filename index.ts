500236de08652f1b27b621e5c50b5e2a35ed6862
Lester Cordero <lester@boss.tech>
2024-05-24 20:19:29 -0600

    v0.2.4

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { createStyled } from "./lib/functions/createStyled";
 export { createTheme } from "./lib/functions/createTheme";
-export { color, hexToRGB, reshade, size } from "./lib/functions/transformers";
+export { color, size, shade, media } from "./lib/functions/transformers";
