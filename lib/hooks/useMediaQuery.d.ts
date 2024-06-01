7e27535cc2219da8bcc62ca10e2bc4b2c6ad2e4a
Lester Cordero <lester@boss.tech>
2024-06-01 17:56:20 -0600

    RC

--- lib/hooks/useMediaQuery.d.ts
+++
@@ -1,4 +0,0 @@
-import { SizesSchema, WithMediaQuery } from "../types";
-export declare const useMediaQuery: (breakpoints?: SizesSchema<number>) => {
-  resolveMediaQuery: <T = any>(values: WithMediaQuery<T>) => any;
-};
