f1e03e17bcbf7b2baf6150061fb1fb4e79fafb53
Lester Cordero <lester@boss.tech>
2024-10-25 23:55:00 -0600

    repacked

--- lib/functions/createTheme.d.ts
+++
@@ -1,7 +0,0 @@
-import { ThemeSchema } from "../types";
-/**
- * Function to create a theme based on a schema.
- * @param schema - Schema with media query values.
- * @returns The compiled schema for use into styled components.
- */
-export declare const createTheme: (schema: ThemeSchema) => ThemeSchema;
--- lib/hooks/useDebouncedState.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * useDebouncedState Hook allows to defer a state update by a timelapse, to rate limit state updates
- */
-export declare const useDebouncedState: (initialValue: any, delay?: number) => any[];
-export default useDebouncedState;
