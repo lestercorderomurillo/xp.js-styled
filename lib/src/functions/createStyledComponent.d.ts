d6cc159b416deb9f0029e2a298b264e8e8da8683
Lester Cordero <lester@boss.tech>
2025-01-26 16:43:56 -0600

    build

--- lib/src/functions/createStyledComponent.d.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { ComponentStyleProps, StyledProps, StyledStyle, Theme } from "../types";
-/**
- * Create a Styled Component given a style schema and a base theme (both optional).
- * @param {React.ComponentType<any>} Component The component to render.
- * @param {StyledStyle} schema The style schema.
- * @returns A wrapper around your component with extended features.
- */
-export declare const createStyled: <TComponent extends React.ComponentType<{}>, TStyleProps = ComponentStyleProps<TComponent>, TVariantNames extends string = never>(Component: TComponent, schema?: StyledStyle<TStyleProps, TVariantNames>) => React.ForwardRefExoticComponent<React.PropsWithoutRef<StyledProps<React.ComponentProps<TComponent>, ComponentStyleProps<TComponent>, keyof StyledStyle<TStyleProps, TVariantNames>["variants"], Theme>> & React.RefAttributes<TComponent>>;
